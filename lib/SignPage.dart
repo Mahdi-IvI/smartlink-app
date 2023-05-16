df9ec43f4063c2c852cac100c8c52694a7b361ad
Mahdi-IvI <m.abbasi1377.ma@gmail.com>
2023-05-17 00:17:14 +0200

    sign page added

---
+++ lib/SignPage.dart
@@ -0,0 +1,26 @@
+
+import 'package:flutter/material.dart';
+
+class SignPage extends StatefulWidget {
+  const SignPage({Key? key}) : super(key: key);
+
+  @override
+  State<SignPage> createState() => _SignPageState();
+}
+
+class _SignPageState extends State<SignPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sign up"),
+      ),
+      body: Column(
+        children: [
+          Text("Sign in and enjoy your smart apartment"),
+
+        ],
+      ),
+    );
+  }
+}
